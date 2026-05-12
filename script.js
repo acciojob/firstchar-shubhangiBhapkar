02636f214d7db6a3a5e421b00cd07cda9243d597
Shubhangi Bhapkar <shubhangibhapkar35@gmail.com>
2026-05-12 16:03:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 function firstChar(text) {
   // your code here
+
+	for(let i=0;i<text.length;i++){
+		if(text.charAt(i)!=" "){
+			return text.charAt(i);
+		}
+		else if(i==text.length-1){
+			return " ";
+		}
+	}
 }
 
 // Do not change the code below
 //Uncomment the following line to show the prompt popup
-//const text = prompt("Enter text:");
+const text = prompt("Enter text:");
 alert(firstChar(text));
+
